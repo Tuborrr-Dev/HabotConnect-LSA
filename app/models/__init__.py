75e672946bcd96994ca11edb7692ec95d1b565c4
Tuborrr <israeladetubo@Israels-MacBook-Pro.local>
2026-08-11 20:43:26 +0100

    Wire up models package so LSASkill association table is registered

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from app.models.parent import Parent
+from app.models.lsa_profile import LSAProfile
+from app.models.skill import Skill
+from app.models.lsa_skill import LSASkill
+from app.models.booking import Booking
+from app.models.payment import Payment
+
+__all__ = ["Parent", "LSAProfile", "Skill", "LSASkill", "Booking", "Payment"]
