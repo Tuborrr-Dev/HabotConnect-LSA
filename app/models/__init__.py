13fa37f96a2ee507e7a60df61eb2cb1a1da23635
Tuborrr <israeladetubo@Israels-MacBook-Pro.local>
2026-08-11 20:47:19 +0100

    Added comments to models to ensure readability

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,3 +6,4 @@ from app.models.booking import Booking
 from app.models.payment import Payment
 
 __all__ = ["Parent", "LSAProfile", "Skill", "LSASkill", "Booking", "Payment"]
+# the above ensures all files are imported and nothing is missing during implementation
--- app/models/skill.py
+++ app/models/skill.py
@@ -3,7 +3,7 @@ from app.extensions import db
 
 class Skill(db.Model):
     __tablename__ = "skills"
-
+    # the primary key is the id column which is the same in allot of other models
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False, index=True)
 
